7b4d3474ac5bc174ae72552ec92078716faf3cc4
Sandesh <sandesh_kumar@epam.com>
2025-09-01 12:40:25 +0530

    added spring boot

---
+++ src/main/java/com/org/inrex/inrex_payment_service/InrexPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.org.inrex.inrex_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InrexPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InrexPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/inrex/inrex_payment_service/InrexPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.inrex.inrex_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InrexPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
